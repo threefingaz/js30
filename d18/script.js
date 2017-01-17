d860de39d730af9db42600175d6123d851dc6013
Al Nikitin <nikitin010@gmail.com>
2017-01-17 18:31:49 +0300

    Day18 "Adding Up Times with Reduce"

---
+++ d18/script.js
@@ -0,0 +1,33 @@
+const time_items = Array.from(document.querySelectorAll('[data-time]'));
+const target = document.querySelector('.total_time');
+
+const totalSeconds = time_items.reduce((total, next) => {
+  const [min, sec] = next.dataset.time.split(':').map(parseFloat);
+  const seconds = min * 60 + sec;
+
+  let secondsLeft = seconds;
+
+  return total + seconds;
+}, 0);
+
+function getHours(seconds) {
+  return Math.floor(seconds / 3600);
+}
+
+function getMinutes(seconds) {
+  seconds = seconds % 3600;
+  return Math.floor(seconds / 60);
+}
+
+function getSeconds(seconds) {
+  return (seconds % 3600) % 60;
+}
+
+const total = {
+  hours  : getHours(totalSeconds),
+  minutes: getMinutes(totalSeconds),
+  seconds: getSeconds(totalSeconds)
+}
+
+
+target.innerHTML = `${total.hours}:${total.minutes}:${total.seconds}`;
